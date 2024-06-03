73d2ab3600f92e91517ad37ae8f62898b5c75936
slavikovab <barbora.slavikova@student.spsmb.cz>
2024-06-03 13:55:32 +0200

    favicon / aboutUs added

---
+++ src/components/AboutUs/AboutUs.jsx
@@ -0,0 +1,58 @@
+import styles from "./AboutUs.module.css";
+
+export default function AboutUs() {
+  return (
+    <>
+      <div className={styles.aboutUs}>
+        <p>O nás</p>
+        <div className={styles.aboutUsBox}>
+          <div className={styles.aboutUsTime}>
+            <h1>Otevírací doba</h1>
+            <p>Pondělí:11:00 až 22:00</p>
+            <p>Úterý: 11:00 až 22:00</p>
+            <p>Středa: 11:00 až 22:00</p>
+            <p>Čtvrtek: 11:00 až 22:00</p>
+            <p>Pátek: 11:00 až 22:00</p>
+            <p>Sobota: 11:30 až 22:00</p>
+            <p>Neděle: zavřeno</p>
+          </div>
+          <div className={styles.aboutUsMap}>
+            <iframe
+              src="https://en.frame.mapy.cz/s/fupukaraze"
+              width="800"
+              height="500"
+              frameborder="0"
+            ></iframe>
+            <p>Adresa: Komenského náměstí 61 29301 Mladá Boleslav</p>
+          </div>
+        </div>
+
+        <div className={styles.aboutUsInfo}>
+          <p>
+            Naše stylová restaurace s americkou BBQ kuchyní nabízí burgery,
+            steaky, pečená žebírka, křidýlka, ryby, saláty, dezerty a především
+            naše domácí hranolky. Každý si u nás přijde na své – jak milovníci
+            masa, tak vegetariáni.
+          </p>
+          <p>
+            Naše kapacita restaurace je 100 míst, z toho jsou k dispozici dva
+            salónky pro pořádání firemních nebo soukromých akcí.
+          </p>
+          <p>
+            Věříme že Vás naše restaurace nadchne svojí kuchyní, designem a
+            příjemnou obsluhou.
+          </p>
+          <p>
+            Každý den pro Vás chystáme polední menu z těch nejkvalitnějších
+            surovin.
+          </p>
+          <p>Naše krédo: kvalita, kvantita, prostě domácí je domácí.</p>
+          <p>
+            Těšíme se na Vaši návštěvu a doufáme, že se Vám trefíme do chutí.
+          </p>
+          <p>Děkujeme – Vaše America „Pod Věží „</p>
+        </div>
+      </div>
+    </>
+  );
+}
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,11 +1,13 @@
 import Navbar from "../../components/Navbar/Navbar"
 import Hero from "../../components/Hero/Hero"
+import AboutUs from "../../components/AboutUs/AboutUs"
 
 export default function Home() {
     return(
         <>
             <Navbar/>
             <Hero/>
+            <AboutUs/>
         </>
     )    
 }
